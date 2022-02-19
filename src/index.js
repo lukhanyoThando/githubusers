4393279ef04ff577c5f0d31f003a74c6a6d319e4
Luhanyo Tando Nkohla <60959721+lukhanyoThando@users.noreply.github.com>
2022-02-19 02:26:48 +0200

    githubusers
    consuming api's

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className='container'>
+    <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
